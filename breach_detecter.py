b66a6825fd89a0792af57e4a2ec7df8d5fa22901
Marshal Revanth C <cr.marshalrevanth@gmail.com>
2023-01-10 19:47:21 +0530

    Create breach_detecter.py
    breach detecter

---
+++ breach_detecter.py
@@ -0,0 +1,8 @@
+from configuration import *
+def infer_breach(value, lowerLimit, upperLimit):
+  if value < lowerLimit:
+    return 'TOO_LOW'
+  if value > upperLimit:
+    return 'TOO_HIGH'
+  return 'NORMAL'
+
